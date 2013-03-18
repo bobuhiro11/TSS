5968fe3962c62fdbf220b1e4d38fb1566dac192b
U,S <uetashunya@Uetas-MacBook-Pro.local>
2013-03-18 14:44:53 +0900

    編入試験試験サイト
    できたてほやほや

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TSS::Application.config.secret_token = '5082038d5dc50cda1c775caa5086a2ee149ad2680fa61dced511f0ad2b709025bb72deaa2dd43096daf1e6998faff48d3457735d519e611fb85d1114295f337b'
