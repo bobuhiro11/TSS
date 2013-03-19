eab064d5e6f6e3e63d425d62b686d76337e5ad23
U,S <uetashunya@Uetas-MacBook-Pro.local>
2013-03-19 10:34:08 +0900

    Revert "deviseつまった"
    This reverts commit a4c72fb9e9baa2f37d3f4e7f5dbc4061e62e068f.

--- app/controllers/transfer_support_system_controller.rb
+++ app/controllers/transfer_support_system_controller.rb
@@ -1,13 +1,8 @@
 class TransferSupportSystemController < ApplicationController
   def index
-    if current_user
-      redirect_to :user_root
-      return
-    end
   end
   def experience_story
   end
-
   def about
   end
 end
