28262ed9bdd20f1a3ee678cf676031b7ae98c94e
bobuhiro11 <blackcherry.mn@gmail.com>
2013-03-25 16:59:15 +0900

    topページを動的に作成するようにした

--- app/controllers/hentai_controller.rb
+++ app/controllers/hentai_controller.rb
@@ -1,5 +1,11 @@
 class HentaiController < ApplicationController
   def index
+    @schools = Story.find_by_sql(
+      "select school_name,count(school_name) as count "+ 
+       "from stories "+
+       "group by school_name "+ 
+       "order by count(school_name) desc;"
+    );
   end
 
   def about
